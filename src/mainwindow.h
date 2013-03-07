29e9f17c16a86e6162fde38eadab17c648ed43f9
Vincent Lee <vincentlee.kr@gmail.com>
2013-03-07 20:09:04 +0900

    Initial commit

---
+++ src/mainwindow.h
@@ -0,0 +1,23 @@
+#ifndef mainwindow_h
+#define mainwindow_h
+
+#include <QMainWindow>
+#include <QScopedPointer>
+
+namespace Ui
+{
+    class MainWindow;
+}
+
+class MainWindow: public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    virtual ~MainWindow();
+
+private:
+    QScopedPointer<Ui::MainWindow> ui;
+};
+
+#endif
