6dc4069afb4c9efcdce5c44f6be07494e483a565
Vincent Lee <vincentlee.kr@gmail.com>
2013-03-07 20:13:47 +0900

    Reindent tabs

--- src/mainwindow.cc
+++ src/mainwindow.cc
@@ -2,10 +2,9 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent),
-      ui(new Ui::MainWindow)
+: QMainWindow(parent), ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
