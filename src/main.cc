a7c4518e0073f4f1a989a49535fb9329f6621406
Vincent Lee <vincentlee.kr@gmail.com>
2014-08-18 20:47:20 +0900

    Add application icon

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,12 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QIcon appIcon;
+    appIcon.addFile(":/Icons/AppIcon32");
+    appIcon.addFile(":/Icons/AppIcon128");
+    app.setWindowIcon(appIcon);
+
     MainWindow mainWindow;
     mainWindow.show();
     return app.exec();
