91cb3c737738cbdda8f6c46d9034694da1d55189
Vincent Lee <vincentlee.kr@gmail.com>
2014-08-18 23:06:42 +0900

    Add comment about Retina display support

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,11 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    // Retina display support for Mac OS, iOS and X11:
+    // http://blog.qt.digia.com/blog/2013/04/25/retina-display-support-for-mac-os-ios-and-x11/
+    //
+    // AA_UseHighDpiPixmaps attribute is off by default in Qt 5.1 but will most
+    // likely be on by default in a future release of Qt.
     app.setAttribute(Qt::AA_UseHighDpiPixmaps);
 
     QIcon appIcon;
