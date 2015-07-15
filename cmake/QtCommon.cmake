5f17138abfa4a4b88e3596facde80369e85220b2
jendas <jendas1@yahoo.com>
2015-07-15 21:40:23 +0200

    Updated add_project_meta syntax

--- cmake/QtCommon.cmake
+++ cmake/QtCommon.cmake
@@ -7,7 +7,7 @@ set(PROJECT_VERSION_TWEAK 0)
 endif()
 endmacro()
 
-macro(add_project_meta)
+macro(add_project_meta FILES_TO_INCLUDE)
 if(NOT RESOURCE_FOLDER)
 set(RESOURCE_FOLDER res)
 endif()
@@ -24,7 +24,10 @@ if (WIN32)
 configure_file("${PROJECT_SOURCE_DIR}/${RESOURCE_FOLDER}/windows_metafile.rc.in"
   "windows_metafile.rc"
 )
-    set(SOURCE_FILES ${SOURCE_FILES} "windows_metafile.rc")
+set(RES_FILES "windows_metafile.rc")
+set(CMAKE_RC_COMPILER_INIT windres)
+ENABLE_LANGUAGE(RC)
+SET(CMAKE_RC_COMPILE_OBJECT "<CMAKE_RC_COMPILER> <FLAGS> -O coff <DEFINES> -i <SOURCE> -o <OBJECT>")
 endif ()
 
 if (APPLE)
@@ -38,7 +41,14 @@ set(MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSI
 set(MACOSX_BUNDLE_COPYRIGHT ${COPYRIGHT})
 set(MACOSX_BUNDLE_GUI_IDENTIFIER ${IDENTIFIER})
 set(MACOSX_BUNDLE_ICON_FILE ${ICON_NAME})
-endif ()
+endif()
+
+if(APPLE)
+set(${FILES_TO_INCLUDE} ${ICON_FILE})
+elseif(WIN32)
+set(${FILES_TO_INCLUDE} ${RES_FILES})
+endif()
+
 endmacro()
 
 macro(init_app_type)
