c5f4b625382361b132c9accf44d5e61ae0df8566
Vincent Lee <vincent@axiomatic-inc.com>
2021-03-04 01:08:33 +0900

    High-DPI scaling is always enabled in Qt 6

--- src/main.cc
+++ src/main.cc
@@ -3,8 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QApplication app(argc, argv);
     MainWindow mainWindow;
     mainWindow.show();
